931e6c7326ad423bea39737c4bea0f2970f36ee3
lamideog <63949461+lamideog@users.noreply.github.com>
2022-05-06 15:40:40 +0100

    Add files via upload

---
+++ plan.js
@@ -0,0 +1,32 @@
+function lagFunction() {
+    var cardOne= document.getElementById("gi")
+    cardOne.src ="imgs/prooo.gif";
+    var cardTwo= document.getElementById("gil")
+    cardTwo.src ="imgs/prov.gif";
+    var cardThree= document.getElementById("gir")
+    cardThree.src= "imgs/pri.gif";
+    var cardFour= document.getElementById("gis")
+    cardFour.src="imgs/card4.gif";
+    var cardFive= document.getElementById("giu")
+    cardFive.src="imgs/card5.gif";
+    var cardSix= document.getElementById("gip")
+    cardSix.src="imgs/card6.gif";
+    var cardSeven= document.getElementById("giq")
+    cardSeven.src="imgs/card7.gif";
+
+}
+function ibFunction() {
+    document.getElementById("gi")
+    .src="imgs/room.jpg";
+    document.getElementById("gil")
+    .src="imgs/room.jpg";
+
+}
+// function myFunction() {
+//     var x = document.getElementById("fun");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
